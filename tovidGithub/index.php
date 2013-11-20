bbdd6169aa87cc0c6d8b72928aeeb855f265c41b
tgh612 <tgh612@uowmail.edu.au>
2013-11-21 10:03:08 +1100

    configuring

--- tovidGithub/index.php
+++ tovidGithub/index.php
@@ -10,7 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        Testing
+        Testing 123
         <?php
         // put your code here
         ?>
