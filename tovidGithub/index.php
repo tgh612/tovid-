2fc2f7d40e616a8dd23e1fc00a1feba0a97099a8
tgh612 <tgh612@uowmail.edu.au>
2013-11-23 10:35:37 +1100

    23/11/2013

--- tovidGithub/index.php
+++ tovidGithub/index.php
@@ -6,13 +6,50 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+       <meta charset=utf-8" />
+        <title>Testing Page</title>
+        
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        
+        
     </head>
-    <body>
-        Testing 123
+    
+    <body id="index" class="home">
+        <div id="container">
+        
+
+            <h1>hello hello</h1>
+            <br>
+            <h1><a href="#">Testing123</a></h1>
+ 
+	<nav><ul>
+		<li class="active"><a href="#">home</a></li>
+                <li><a href="link1.php">link1</a></li>
+                <li><a href="link2.php">link2</a></li>
+
+	</ul></nav>
+            
+            <div id="col1">
+                <p>fuckfuck</p>
+            </div>
+            <div id="col2">
+                <P>qqqqqqq</P>
+            </div>
+            
+            <div id="col2outer">
+                <div id="col2mid">
+                    <p>wwwwww</p>
+                </div>
+            <div id="coll2side">
+                <p>lllllll</p>
+            </div>
+            </div>
+        </div>
         <?php
         // put your code here
+  
         ?>
+        
     </body>
 </html>
