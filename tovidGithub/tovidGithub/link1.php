41b1a6f6cd687979e46ef2dd9278609e1456c017
tgh612 <tgh612@uowmail.edu.au>
2013-11-23 10:39:03 +1100

    more test

---
+++ tovidGithub/tovidGithub/link1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>link1</title>
+        
+        <link rel="stylesheet" href="style.css" type="text/css" />
+        
+    </head>
+
+
+<body id="link1" class="link1">
+ link 1
+ <?php
+
+?>  
+</body>
+</html>
+
+
+
---
+++ tovidGithub/tovidGithub/tovidGithub/tovidGithub/tovidGithub/link2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>link2</title>
+        
+        <link rel="stylesheet" href="style.css" type="text/css" />
+        
+    </head>
+
+
+<body id="link2" class="link2">
+ link 2
+ <?php
+
+?>  
+</body>
+</html>
+
+
+
